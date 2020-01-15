84cb7924c3415b8b604ba9b0b5cca4ca4c6767bb
DGC75 <gabrielecongiu75@gmail.com>
2020-01-15 23:17:51 +0100

    l10e1

--- PR1/prlb2019_2020/l10/1_inserimento_lista_rec/box.c
+++ PR1/prlb2019_2020/l10/1_inserimento_lista_rec/box.c
@@ -1,22 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct intero{
-    int intero;
+    int dato;
     struct intero* next;
 }Intero;
 
 typedef Intero* ListaInteri;
 
+void insert_h(ListaInteri *lPtr, int data);
+void insert_negativi(ListaInteri *lPtr);
+void print_lst(ListaInteri lPtr);
+void mem_error();
+
+
 int main(void){
 
+	int input = 0;
+	ListaInteri lst = NULL;
+
+	while(scanf("%d", &input) == 1 && input >= 0){
+		insert_h(&lst, input);
+	}
+	insert_negativi(&lst);
+	print_lst(lst);
 
     return 0;
 }
 
+void print_lst(ListaInteri lPtr){
+	while(lPtr != NULL){
+		printf("%d -> ", lPtr->dato);
+		lPtr = lPtr->next;
+	}
+	printf("NULL\n");
+}
+
+void insert_h(ListaInteri *lPtr, int input){
+	Intero *newIntero = calloc(1, sizeof(Intero));
+
+	if(newIntero == NULL)
+		mem_error();
+
+	newIntero->dato = input;
+	newIntero->next = *lPtr;
+
+	*lPtr = newIntero;
+
+}
+
 void insert_negativi(ListaInteri *lPtr){
+
+    ListaInteri *head = lPtr;
+    ListaInteri *tail = NULL;
+
     if(*lPtr != NULL){
         if((*lPtr)->next != NULL){
-            
+     		tail = &(*head)->next;	
+     		insert_negativi(tail);       
         }
+    	if ((((*head)->dato)%2) == 0){
+
+    		Intero *newIntero = calloc(1, sizeof(Intero));
+
+    		if (newIntero == NULL)
+    			mem_error();
+
+    		newIntero->dato = -1;
+
+    		newIntero->next = *head;
+    		(*head) = newIntero;
+    	}
+    	else if(tail != NULL){
+    		(*head)->next = *tail;
+    		return;
+    	}
     }
+}
+
+void mem_error(){
+	puts("Memoria esaurita");
+	exit(EXIT_FAILURE);
 }
