db4b137f81423ed7cbfef3d3c49b880af54dedd5
DGC75 <gabrielecongiu75@gmail.com>
2020-01-15 10:22:19 +0100

    wip

--- PR1/esami_precedenti/2018-12-18/box.c
+++ PR1/esami_precedenti/2018-12-18/box.c
@@ -19,21 +19,27 @@ typedef struct automa{
     
 } Automa;
 
-void acquisisci_automa(Automa** aPtr);
-int  riconosci_str(Automa* aPtr, char *str);
+int acquisisci_automa(Automa** aPtr);
+int  riconosci_str(Automa* aPtr, char *str, int dim);
 void insert(ListaTransizioni *lPtr, int next_state, char accepted_c);
+int find_in_list(Automa *aPtr, int state, char c);
+int is_in(int val, int *arr, int dim);
+
+
 void saveInt(int *val);
 void errore_input();
+void mem_error();
+
 
 int main(){
 
     Automa *automa = NULL;    
-    acquisisci_automa(&automa);
-    
+    int dim = acquisisci_automa(&automa);
+
     char line[101];
-    fgets(line, 101, stdin);
-    
-    if(riconosci_str(automa, line))
+    scanf("%[^\n]", line);
+    printf("'%s'\n", line);
+    if(riconosci_str(automa, line, dim))
         puts("Stringa appartenente al linguaggio.");
     else
         puts("Stringa non appartenente al linguaggio.");
@@ -42,7 +48,7 @@ int main(){
     return 0;
 }
 
-void acquisisci_automa(Automa** aPtr){
+int acquisisci_automa(Automa** aPtr){
 
     Automa *newAutoma = calloc(1, sizeof(Automa));
     int n_states, n_final_states;
@@ -68,16 +74,46 @@ void acquisisci_automa(Automa** aPtr){
         }
         
     }
+
+    return n_final_states;
 }
 
 
-int  riconosci_str(Automa* aPtr, char *str){
+int  riconosci_str(Automa* aPtr, char *str, int dim){
+    int i = 0;
+
+    int curr_state = 0;
+    char c = str[i];
+    for(i = 1; c!='\0' && c!='\n'; ++i){
+        
+        curr_state = find_in_list(aPtr, curr_state, c);
+        c = str[i];
+    }
+
+    return is_in(curr_state, aPtr->arr_stati_finali, dim);
+}
+
+int find_in_list(Automa *aPtr, int state, char c){
+    return 1;
+}
+
+int is_in(int val, int *arr, int dim){
     return 1;
 }
 
 void insert(ListaTransizioni *lPtr, int next_state, char accepted_c){
-    
+
+    Transizione *newTrans = calloc(1, sizeof(Transizione));
+
+    if (newTrans == NULL)
+        mem_error();
+
+    newTrans->next = *lPtr;
+    (*lPtr) = newTrans;
 }
+
+
+
 
 void saveInt(int *val){
     if(scanf("%d", val) != 1 || *val < 0 )
@@ -86,5 +122,10 @@ void saveInt(int *val){
 
 void errore_input(){
     puts("Errore input");
+    exit(EXIT_FAILURE);
+}
+
+void mem_error(){
+    puts("Memoria esaurita");
     exit(EXIT_FAILURE);
 }
