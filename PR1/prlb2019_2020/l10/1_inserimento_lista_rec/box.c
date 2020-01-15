e6c2fa311ec9cf180b70e32e4c5fe0d582745648
GABRIELE CONGIU <g.congiu2@goldUbuntu>
2020-01-15 18:45:42 +0100

    l10.1

--- PR1/prlb2019_2020/l10/1_inserimento_lista_rec/box.c
+++ PR1/prlb2019_2020/l10/1_inserimento_lista_rec/box.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+typedef struct intero{
+    int intero;
+    struct intero* next;
+}Intero;
+
+typedef Intero* ListaInteri;
 
 int main(void){
 
 
+    return 0;
+}
+
+void insert_negativi(ListaInteri *lPtr){
+    if(*lPtr != NULL){
+        if((*lPtr)->next != NULL){
+            
+        }
+    }
 }
