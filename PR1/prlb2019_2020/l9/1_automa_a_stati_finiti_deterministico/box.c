81fb8b8d46f3fa23b89b8a0026bdee0564b515e2
GABRIELE CONGIU <g.congiu2@goldUbuntu>
2020-01-15 17:55:58 +0100

    Automatron completato :)

--- PR1/prlb2019_2020/l9/1_automa_a_stati_finiti_deterministico/box.c
+++ PR1/prlb2019_2020/l9/1_automa_a_stati_finiti_deterministico/box.c
@@ -2,170 +2,218 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_DIM 100
 
-typedef enum bool{
-	false = 0,
-	true  = 1
-}Bool;
+#define MAX_STR 100
 
 typedef struct transizione{
-
-	char *next_state;
-	char acceptable_char;
-	struct transizione* next_trans;
-
+    
+    char acceptable_char;
+    char* next_state;
+    
+    struct transizione* next;
+    
 }Transizione;
 
-typedef Transizione* ListaTransizioni;
+typedef Transizione* Lista_Transizioni;
 
 typedef struct stato{
-	char *nome;
-	ListaTransizioni lista_transizioni;
+    
+    char *nome_stato;
+    Lista_Transizioni lst_trans_stato;
+    
+    struct stato* next;
+            
 }Stato;
 
-void errore_input(void);
-void mem_error(void);
+typedef Stato* Lista_stati;
 
-void salva_int(int *input);
-int lunghezza_stringa(FILE* file_input);
-void aggiungi_nuovo_stato(Stato* stati_automa);
-void insert_ListaTransizioni(ListaTransizioni *lPtr, Transizione* newTrans);
-Bool string_belong(char *str, int dim, Stato *stati_automa);
-void set_current_state(char** current_state, ListaTransizioni lPtr);
+typedef struct automa{
+    
+    int n_stati;
+    Lista_stati lst_stati;
+        
+}Automa;
 
-int is_in();
+void acquisisci_automa(Automa **aPtr);
+int  str_belong(char * str, Automa *aPtr);
+char* get_new_state(char c, char* current_state, Lista_stati lPtr);
+void insert(Lista_Transizioni *lPtr, Transizione **trans);
+void insert_state(Lista_stati *lsPtr, Stato **sPtr);
 
-int main(void){
+void cleanBuffer(char *str, int dim);
+void mem_error();
+void save_int(int* val);
+void errore_input();
 
-	int i, n_stati;
-	salva_int(&n_stati);
+int main(){
+    
+    Automa* automa = NULL;
+    
+    acquisisci_automa(&automa);
+    
+    char riga[MAX_STR +1];
+    
+    fgets(riga, MAX_STR+1, stdin);
+    riga[strlen(riga)-1] = '\0';
+    
+    while( (strcmp(riga, "FINE") != 0) ){
+        
+        if(str_belong(riga, automa)){
 
-	Stato stati_automa[n_stati];
-
-	for (i = 0; i < n_stati; ++i){
-		aggiungi_nuovo_stato(&stati_automa[i]);	
-	}
-
-	char riga[MAX_DIM+1];
-	while(fgets(riga, MAX_DIM+1, stdin) != NULL && (strcmp(riga,"FINE") != 0)){
-		if (string_belong(riga, MAX_DIM, stati_automa)){
-			printf("%s\n", riga);
-		}
-	}
-	
-	return 0;	
+            printf("%s\n", riga);
+        }
+        fgets(riga, MAX_STR+1, stdin);
+        riga[strlen(riga)-1] = '\0';
+    }
+    
+    return 0;
 }
 
-void aggiungi_nuovo_stato(Stato* stato_automa){
+void acquisisci_automa(Automa **aPtr){
+    
+    int n_stati;
+    int i;
+    
+    Automa *newAutoma = calloc(1, sizeof(Automa));
+    if(newAutoma == NULL)
+        mem_error();
+        
+    save_int(&n_stati);
+    char string[MAX_STR] = {'\0'};
+    
+    Stato *newStato = NULL;
 
-	static int str_len;
-	static char *token;
+    for(i = 0; i < n_stati; ++i){
+        
+        
+        scanf("%[^;]", string);
 
-	str_len = lunghezza_stringa(stdin);
-	char riga[str_len];
-	fgets(riga, str_len+1, stdin);
-	riga[str_len-1] = '\0';
-	printf("riga: '%s'\n", riga);
-	token = strtok(riga, ";(,)");
-
-	if (token != NULL){
-
-		stato_automa = calloc(1, sizeof(Stato));
-
-		if (stato_automa == NULL){
-			mem_error();
-		}
-			
-		stato_automa->nome = token;
-		printf("%s\n", stato_automa->nome);
-		token = strtok(NULL, ";(,)");
-			
-		while(token != NULL){
-
-			Transizione* newtrans = calloc(1, sizeof(Transizione));
-
-			if (newtrans == NULL){
-				mem_error();
-			}
-			else{
-				newtrans->next_state = token;
-
-				token = strtok(NULL, ";(,)");
-				newtrans->acceptable_char = *token;
-
-				/*INSERISCO LA TRANSIZIONE NELLA LISTA*/
-				insert_ListaTransizioni(&stato_automa->lista_transizioni, newtrans);
-				token = strtok(NULL, ";(,)");
-			}
-		}
-	}
-
-}
-
-Bool string_belong(char *str,int dim, Stato *stati_automa){
-	
-	int i;
-	char *current_state = stati_automa[0].nome;
-	
-	for(i = 0; i < dim; ++i){
-	
-	    set_current_state(&current_state , stati_automa[i].lista_transizioni);
-	}
-	
-	if(current_state[0] == 'F')
-	    return 1;
-	else
-	    return 0;
+        newStato = calloc(1, sizeof(Stato));
+        newStato->nome_stato = calloc(strlen(string)+1, sizeof(char));
+        strcpy(newStato->nome_stato, string);
+        
+        
+        cleanBuffer(string, MAX_STR);
+        static char tmp_c;
+        
+        
+        while(scanf(";(%[^,],%c)", string, &tmp_c) == 2){
+            
+            Transizione *newTrans = calloc(1, sizeof(Transizione));
+            newTrans->acceptable_char = tmp_c;
+            newTrans->next_state = calloc( strlen(string) + 1, sizeof(char));
+            strcpy(newTrans->next_state, string);
+            
+            insert(&(newStato->lst_trans_stato), &newTrans);
+                                
+        }
+        
+        insert_state(&(newAutoma->lst_stati), &(newStato));
+        
+        scanf("%*c");   
+    }
+        
+    *aPtr = newAutoma;
 }
 
 
-
-void set_current_state(char** current_state, ListaTransizioni lPtr){
-
+void insert_state(Lista_stati *lsPtr, Stato **sPtr){
+    
+    if(*lsPtr == NULL){
+        *lsPtr = *sPtr;
+    }
+    else{
+        while((*lsPtr)->next != NULL){
+            lsPtr = &(*lsPtr)->next;
+        }
+        (*lsPtr)->next = *sPtr;
+        
+    }
+    
 }
 
-void insert_ListaTransizioni(ListaTransizioni *lPtr, Transizione* newTrans){
-	if (*lPtr == NULL){
-		*lPtr = newTrans;
-	}
-	else{
-		ListaTransizioni tmpPtr = (*lPtr);
-		*lPtr = newTrans;
-		newTrans->next_trans = tmpPtr;
-
-	}
+void insert(Lista_Transizioni *lPtr, Transizione **trans){
+    
+    if(*lPtr == NULL){
+        *lPtr = *trans;
+    }
+    else{
+        while((*lPtr)->next != NULL){
+            lPtr = &(*lPtr)->next; 
+        }
+        (*lPtr)->next = *trans;
+    }
+    
 }
 
-void mem_error(void){
-	puts("memoria esaurita.");
-	exit(EXIT_FAILURE);
+
+void cleanBuffer(char *str, int dim){
+    int i;
+    for(i = 0; i < dim; ++i){
+        str[i] = '\0';
+    }
 }
 
-int lunghezza_stringa(FILE* file_input){
 
-	int result = 0;
+int str_belong(char * str, Automa *aPtr){
+    
+    int str_size = strlen(str);
+    int i;
+    
+    
+    
+    char *curr_state = aPtr->lst_stati->nome_stato;
+    
+                   
 
-	fpos_t  start_pos;
-	fgetpos(stdin, &start_pos);
+    for(i = 0; i < str_size; ++i){
 
-	char c = fgetc(stdin);
-	while(c != '\n' && c != EOF){
-		++result;
-		c = fgetc(stdin);
-	}
-	fsetpos(stdin, &start_pos);
-	return result+1;
+        curr_state = get_new_state(str[i], curr_state, aPtr->lst_stati);    
+        if(curr_state[0] == '\0'){
+            return 0;
+        }        
+    }
+    
+    if(curr_state[0] == 'F')
+        return 1;
+    else
+        return 0;
 }
 
-void salva_int(int *input){
-	if (scanf("%d", input) != 1 || *input < 1){
-		errore_input();
-	}
-	scanf("%*c");
+char* get_new_state(char c, char* curr_state, Lista_stati lPtr){
+    while(lPtr != NULL){
+        
+        if(strcmp(lPtr->nome_stato, curr_state) == 0){
+            Transizione* tPtr = lPtr->lst_trans_stato;
+            while(tPtr != NULL){
+                
+                if(c == tPtr->acceptable_char)
+                    return tPtr->next_state;
+                
+                tPtr = tPtr->next;
+            }
+        }
+        
+        lPtr = lPtr->next;
+    }
+    
+    return "";
 }
 
-void errore_input(void){
-	puts("input non corretto");
-	exit(EXIT_FAILURE);
+void save_int(int *val){
+    if(scanf("%d", val) != 1 || *val < 1 )
+        errore_input();
+    
+    scanf("%*c");    
 }
+
+void mem_error(){
+    puts("Memoria esaurita");
+    exit(EXIT_FAILURE);
+}
+
+void errore_input(){
+    puts("Errore nell'input");
+    exit(EXIT_FAILURE);
+}
+
