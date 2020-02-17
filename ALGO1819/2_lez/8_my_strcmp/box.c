970fa61b5196ab57868cef7bb22b6f06536b08c9
DGC75 <gabrielecongiu75@gmail.com>
2020-02-17 22:00:06 +0100

    wip

--- ALGO1819/2_lez/8_my_strcmp/box.c
+++ ALGO1819/2_lez/8_my_strcmp/box.c
@@ -1,27 +1,9 @@
-//SALVA STR1
-//SALVA STR2
-
-//STAMPA OUTPUT strcmp(str1, str2);
+#include <stdio.h>
 
 
-//strcmp(str1, str2);
-    //se str1 <= str2
-        a = str1
-        b = str2
-    //altrimenti
-        a = str2
-        b = str1
-    
-    //FINCHE' NON SIAMO ALLA FINE DI STR1
-        if a[i] > b[i]
-            return 1;
-        else if a[i] < b[i]
-            return -1;
-        else
-            i++;     
-    
-    if a < b 
-        return -1;
-    else if a > b
-        return 1;
-        
+int main(){
+    char *str1 = get_string(stdin);
+    char *str2 = get_string(stdin);
+
+    printf("%d\n", mystrcmp);
+}
